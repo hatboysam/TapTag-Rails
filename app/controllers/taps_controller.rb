92bca56ab30c8294e55db6b461a43aef00406435
Sam Stern <hatboysam@gmail.com>
2012-08-17 01:36:37 -0400

    Fix for Tapped-Time submission

--- app/controllers/taps_controller.rb
+++ app/controllers/taps_controller.rb
@@ -1,7 +1,15 @@
 class TapsController < ApplicationController
 
 	def create
+		#Parse DateTime from UNIX Timestamp
+		if (params[:tap][:tapped_time])
+			@tapped_time = params[:tap][:tapped_time]
+			@parsed = Time.at(@tapped_time / 1000).to_datetime
+			params[:tap][:tapped_time] = @parsed
+		end
+		#Build Tap
 		@tap = Tap.new(params[:tap])
+		#Time is now if not stated
 		if (@tap.tapped_time.nil?)
 			@tap.tapped_time = DateTime.now
 		end
